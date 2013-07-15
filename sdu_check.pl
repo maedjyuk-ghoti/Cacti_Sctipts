13ce4798eae4a444c0b11c1c9fad25f77bcae88d
zclouds <s.edwin.green@gmail.com>
2013-07-15 12:33:34 -0400

    minor fixes
    updated sdu_check.pl and removed sdu_check_test_2.pl and qfs1-sdu.infile

--- sdu_check.pl
+++ sdu_check.pl
@@ -2,177 +2,166 @@
 
 use strict;
 use warnings;
+use Sys::Hostname; #used for 'my $metadata_server = hostname();'
 
 ###############################################################################
-# FILE:  	sdu_check.pl
+# FILE:         sdu_check.pl
 #
-# CREATED:	06/03/2013
-# UPDATED:	06/10/2013
+# CREATED:      06/03/2013
+# UPDATED:      06/10/2013
 #
-# DESC:		This script is used for a nightly cronjob to the previous days sar
-# Usage:	sdu_check.pl <filesystem_name> <optional-copy_old_datafile>
+# DESC:         This script is used for a nightly cronjob to the previous days sar
+# Usage:        sdu_check.pl <filesystem_name> <optional-copy_old_datafile>
 ###############################################################################
 
 ###############################################################################
 # Step 1 - Perform initializations.
 ###############################################################################
 
-# TODO -
+my $metadata_server = hostname();
+my $prog_name = $0; 	         #Program name, e.g. putsar.sh
+my ($seconds,$minutes,$hours,$mday,$month,$year,$wday,$yday,$isdst) = localtime();
+$year += 1900; # needed to adjust year appropriately
+my @days = qw/Sun Mon Tue Wed Thu Fri Sat/;
+my $stime = $days[$wday] . " " . join("/", $month, $mday, $year) . ", " . join(":", $hours , $minutes);
+my $tstamp = "$year$month$mday" . "_" . "$hours$minutes$seconds";
+my $logfile = "/var/tmp/sdu_check.pl_" . $tstamp;
+my $fsdir = '/usr/local/data';
+my $fsname = $ARGV[0];
+my $inputfile = "/usr/local/data" . $fsname . "-sdu.infile";
 
-my $metadata_server = `/usr/ucb/hostname`;
-my $prog_name = `basename $0`;          #Program name, e.g. putsar.sh
-my $stime = `date +"%a %D, %I:%M%p"`;   # Format: Thu 05/05/99, 4:44PM
-my $tstamp = `date +"%Y%m%d_%H%M%S"`;
-my $logfile = "/var/tmp/" . $prog_name . "_" . $tstamp;
+open (inputfile_handle, "< $inputfile") || die "Couldn't open $inputfile for read: $!\n";
+open (logfile_handle, "> $logfile") || die "Couldn't open $logfile for append: $!\n";
 
-my $fsdir = "/usr/local/data/";
-my $fsname = $1;
-my $inputfile = $fsdir . $fsname . '-sdu.infile';
-my $td_datafile = $fsdir . $fsname . '_sdu_data.csv';
-print "td_datafile is $td_datafile";
-my $title_page = $fsdir . $fsname . '-sdu_title';
-my $ftp_input_file = "/usr/local/data/" . $fsname . '-sdu_ftp.infile';
-print "ftp file is $ftp_input_file";
-
-touch $logfile;
-system("exec >> $logfile 2>&1"); # Append stdout & stderr to logfile # TODO - FIX THE FOLLOWING LINE
-#open ($logfile_handle, '>>', $logfile) or die ("Can't open $logfile for append: $!");
-print "Starting program $prog_name $stime";
-print "fsdir is $fsdir";
-print "fsname is $fsname";
-print "ftp file is $ftp_input_file";
-print "td_datafile is $td_datafile";
-print "inputfile is $inputfile";
-print "title is $title_page";
-print "Input file for program is  $inputfile";
+my_print("Starting program $prog_name $stime\n");
+my_print("fsdir      is $fsdir\n");
+my_print("fsname     is $fsname\n");
+my_print("inputfile  is $inputfile\n");
 
 #################################################################################
 # Step 2 - Validate Input Parameters and create string containing dates the
 #          script needs to create reports for.
 #################################################################################
 
-# TODO - fix to print filesystem_name, and to print it everytime it scans a new file system.
+# TODO -
 
 if (@ARGV != 1) {
-	print "you must enter a name to distinguish which filesystem";
+    my_print("you must enter a name to distinguish which filesystem\n");
+	exit 1;
 }
-if (@ARGV == 2) {
-	cp $td_datafile $2;
-	cat $title_page > $td_datafile;
-}
-my $todaysdate = `date +"%Y-%m-%d"`;
-my $inputstr = $todaysdate . ",";
+my $todaysdate = join("-", $year, $month, $mday);
 my $count = 0;
-foreach my $dir(`cat $inputfile`) {
-	
-	#Skip the line if commented out.
-	if (index($dir,"#") != -1) {
-		#nothing happens here.
+while (<inputfile_handle>) {
+	chomp($_);
+	my $inputstr = $todaysdate . ",";
+
+	#Skip to next line in $inputfile if current one is commented out.
+ 	next if /^#/;
+
+	#Parse the line gathered from $inputfile
+	my @oid_pathname = split(':', $_);
+	my $oid = $oid_pathname[0];
+	my @path = split('/', $oid_pathname[1]);
+
+	#@path[0] should always be empty due to the syntax of the input file.
+	if (($path[0] ne "") || ((scalar @path) < 2)) {
+		my_print("Invalid path name: $path[0]\n");
 	} else {
-		
-		##########################
-		# Input file should be in the following format:
-		#  <OID_Number>:<FileSystem>
-		#  <OID_Number>:<FileSystem>/<Directory>
-		#  (etc.)
-		#
-		# You can comment out a line, signalling this program to skip that line,
-		#  by having two crunch/pound (##) anywhere in the line, although it is
-		#  preferred at the beginning of the line.
-		#
-		# In the example below, lines 3, 6, 7, and 8 are commented out.
-		#
-		# Example:
-		#  4.3.2.1.1:/A_File_System
-		#  4.3.2.1.2:/A_File_System/A_Directory1
-		#  ##4.3.2.1.3:/A_File_System/A_Directory2
-		#  4.3.2.2.1:/B_File_System
-		#  4.3.2.2.2:/B_File_System/B_Directory1
-		#  ##:/B_File_System/B_Directory2
-		#  4.3.2.2.3#:/B_File_System/B_Directory3
-		#  4.3.2.2.4:/B_File_System##/B_Directory4
-		##########################
-		
-		#parse the line gathered from $inputfile
-		my @oid_pathname = split(/:/, $dir);
-		my $oid = $oid_pathname[0];
-		my @path = split(/\//, $oid_pathname[1]);
-		
-		#@path[0] should always be empty due to the syntax of the input file.
-		if (($path[0] ne "") || ((scalar @path) < 2)) {
-			print "Invalid path name: $path";
-		} else if ((scalar @path) == 2) {
+		if ((scalar @path) == 2) {
 			#if line is a file system name, print it out
-			print "fsname is $fsname:  oid is $oid";
-		} else {
-			my $cmdoutput = `/opt/SUNWsamfs/bin/sdu -s $dir`;
-			my $dirsize = `echo $cmdoutput | awk '{echo $1}'`;
-			$inputstr = $inputstr . $dirsize . ",";
-			print "dir is $dir,size is $dirsize";
-			print "inputstr is $inputstr";
-			$count += 1;
-			
-			my $dirname = $path[-1];
-			print "count is $count, dirname is $dirname";
-			print "Directory is: $dirname found size is $dirsize";
-			print "Collecting data for directory $dirname oid is $oid";
-			
-			# ds_collect_update.pl ######################################
-			#system("/usr/local/src/ds_collect_update.pl", $dirsize, $dirname, INTEGER, $oid);
-			# Convert Data
-			my $gigs = int ($dirsize / 1024 / 1024);
-
-			# debug
-			#print "argv1=$dirsize \n";
-			#print "gigs=$gigs \n";
-			# end debug
-
-			if ($gigs < 1) {
-			  $gigs = 1;
-			}
-
-			# Break down OID
-			my @oidtemp = split (/\./, $oid);
-			pop(@oidtemp);
-			my $oidDir = join('/',@oidtemp);
-			my @oidValues = split(/\./, $oid);
-			my $fileOID = $oidValues[-1];
-
-			# debug
-			#print "dirOID:$oidDir \n";
-			#print "fileOID:$fileOID \n";
-			# end debug
-
-			# create directory structure if needed
-			my $dirother = "/var/run/collect/$oidDir";
-
-			# debug
-			#print "dir:$dirother \n";
-			# end debug
-
-			if (!(-d $dirother)) {
-			   # debug
-			   #print "dir does not exist\n";
-			   # end debug
-			   system("mkdir -p $dirother");
-			}
-			# Open file and write data to it
-			open FH, ">/var/run/collect/$oidDir/$fileOID" or die "Can't open file /var/run/collect/$oidDir/$fileOID\n";
-			my $data = "$fileOID:$2:$3:$gigs";
-
-			# debug
-			#print "$data \n";
-			# end debug
-
-			print FH $data;
-			close FH;
-			# end ds_collect_update.pl ############################################
-
-			print "Finished collecting data for directory $dirname";
+			my_print("fsname     is $fsname\n");
+			my_print("oid        is $oid\n");
 		}
+
+		# debug
+		#print "Running sdu -s $oid_pathname[1]\n";
+		# end debug
+
+		my $cmdoutput = `/opt/SUNWsamfs/bin/sdu -s $oid_pathname[1]`;
+
+		# debug
+		#print "Running awk '{print \$1}' on cmdoutput\n";
+		#print "$cmdoutput\n";
+		# end debug
+
+		my @temp = split(' ', $cmdoutput);
+		my $dirsize = $temp[0];
+		$inputstr = $inputstr . $dirsize . ",";
+		my_print("dir        is $oid_pathname[1]\n");
+		my_print("size       is $dirsize\n");
+		my_print("inputstr   is \"$inputstr\"\n");
+		$count += 1;
+		my $dirname = $path[-1];
+		my_print("count      is $count\n");
+		my_print("dirname    is $dirname\n");
+		my_print("Directory  is $dirname\n");
+		my_print("Found size is $dirsize\n");
+		#my_print("oid        is $oid\n");
+		# Convert Data
+		my $gigs = ($dirsize / 1024.0 / 1024.0);
+		my $gigs_str = sprintf("%.2f", $gigs);
+
+		# debug
+		#print "argv1=$dirsize\n";
+		#print "gigs=$gigs_str\n";
+		# end debug
+
+		if ($gigs < 1) {
+		#  $gigs = 1;
+		}
+		# Break down OID
+		my @oidtemp = split ('\.', $oid);
+		pop(@oidtemp);
+		my $oidDir = join('/',@oidtemp);
+		my @oidValues = split('\.', $oid);
+		my $fileOID = $oidValues[-1];
+
+		# debug
+		#print "dirOID:$oidDir\n";
+		#print "fileOID:$fileOID\n";
+		# end debug
+
+		# create directory structure if needed
+		my $dirother = "/var/run/collect/$oidDir";
+
+		# debug
+		#print "dir:$dirother \n";
+		# end debug
+
+		if (!(-d $dirother)) {
+			# debug
+			#print "dir does not exist\n";
+			# end debug
+
+			system("mkdir -p $dirother");
+		}
+		# Open file and write data to it
+		open FH, ">/var/run/collect/$oidDir/$fileOID" or die "Can't open file /var/run/collect/$oidDir/$fileOID\n";
+		my $data = join(":", $fileOID, $dirname, "INTEGER", $gigs_str);
+
+		# debug
+		#print "$data \n";
+		# end debug
+
+		print FH $data;
+		close FH;
+		# end of ds_collect_update.pl
+
+		my_print ("Finished collecting data for directory $dirname\n");
+		#if (@path == 2) {
+		#	my_print ("fsname is $fsname:  oid is $oid\n");
+		#}
 	}
 }
-print "fsname is $fsname:  oid is $oid";
-print "$inputstr" >> $td_datafile;
 
-#end of script
+close logfile_handle;
+#end of main script
+
+#Subroutines/Functions
+# my_print
+#  Desc:	Prints to stdout and another stream
+sub my_print {
+	print $_[0];
+	print logfile_handle $_[0];
+}
+
