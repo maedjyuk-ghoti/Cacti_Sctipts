15d783b42c69dc2e98e700a4cd3ece6d98f1cd2a
zclouds <s.edwin.green@gmail.com>
2013-07-22 14:43:47 -0400

    fixed string parsing
    Still have a to fix something to do with math and itc_free_tape and
    poole_free_tape.

--- freetape.pl
+++ freetape.pl
@@ -26,16 +26,18 @@ use Switch 'Perl5';
 # Example: "samqfs12:ITC:B:200:100:C:20:5"
 
 #Input file location
-my $input_dir = "/qfs5_misc/qfscfg/freetape/";
+my $input_dir = "/qfs5_misc/qfscfg/freetape";
+#Directory for old Input files
+my $old_dir = "/qfs5_misc/qfscfg/freetape/old";
 #OID directory info
 my $storage_dir = "/var/run/collect/4/3/2/0";
 #Add extra hosts here
-my $free_tape_itc   = "/0/1";
-my $free_tape_poole = "/0/2";
+my $free_tape_itc   = "/0";
+my $free_tape_poole = "/0";
 my $samqfs          = "/1";
 my $sancon          = "/2";
 #Offset value to make dir for tape type
-my $a_value = (ord('a') - 1);
+my $a_val_adj = (ord('A') - 1);
 #Error constants
 use constant {
 	NAME         => 1,
@@ -47,7 +49,9 @@ use constant {
 #Create date stamp
 my ($seconds,$minutes,$hours,$mday,$month,$year,$wday,$yday,$isdst) = localtime();
 $year += 1900; # needed to adjust year appropriately
-my $date = $year.$month.$mday;
+$month = $month + 1;
+my $date = $year."0".$month.$mday;
+print "today is: $date\n";
 #Important data to determine free tape per library
 my $itc_tape_total;
 my @itc_tape_alloc;
@@ -59,21 +63,25 @@ my @poole_tape_used;
 my @files = <$input_dir/*>;
 foreach my $file (@files) {
 	#Check file names
-	my @file_split = split('_', $file);
+	my @dir = split('/', $file);
+if ($dir[-1] eq "old") {
+next;
+}
+	my @file_split = split('_', $dir[-1]);
 	if (@file_split != 2) {
-		error_condition(NAME, $file);
+		error_condition(1, $file);
 		next;
 	}
+if ($file_split[0] eq "devedwdi1") {
+next;
+}
 	my $hostname  = $file_split[0];
 	my $file_date = $file_split[1];
-	#Check date form
-	if ($file_date ne \d{8}) {
-		error_condition(DATE_FORM, $file_date);
-		next;
-	}
+
 	#Check for today's date
-	elsif ($file_date ne $date) {
-		error_condition(DATE_OUT, $file_date);
+	if ($file_date ne $date) {
+		print "moving $file to old directory\n";
+	#	system ("mv $file $old_dir");
 		next;
 	}
 	open (infile_handle, "< $file") || die "Couldn't open $file for read: $!\n";
@@ -81,39 +89,43 @@ foreach my $file (@files) {
 		chomp($line);
 		my @info = split(':', $line);
 		if (((@info - 2) % 3) != 0) {
-			error_condition(INFO_ERR, $line);
+			error_condition(4, $line);
 			next;
 		}
 		if ($hostname ne $info[0]) {
-			error_condition(HOSTNAME_DIF, $hostname, $info[0]);
+			error_condition(5, $hostname, $info[0]);
 			next;
 		}
 		#tape_types is the number of tapes whose info is given on the line
 		my $tape_types = ((@info - 2) / 3);
 		my $location   = $info[1];
+		print "$location, $hostname\n";
 		my @tape_name;
 		my @tape_alloc;
 		my @tape_used;
-		for (my $i = 1; $i <= $tape_types; $i++) {
-			push (@tape_name, ($info[($tape_types * $i) + 2]));
-			push (@tape_alloc, ($info[($tape_types * $i) + 3]));
-			push (@tape_used, ($info[($tape_types * $i) + 4]));
-			if ($hostname ne "sancon[23]") {
+		for (my $i = 0; $i < $tape_types; $i++) {
+			push (@tape_name, ($info[(3 * $i) + 2]));
+			push (@tape_alloc, ($info[(3 * $i) + 3]));
+			push (@tape_used, ($info[(3 * $i) + 4]));
+			if ($hostname eq "sancon2" || $hostname eq "sancon3") {
+				print "WHOOP! THERE IT IS!\n";
 				if ($location eq "itc") {
-					push (@itc_tape_alloc, ($info[($tape_types * $i) + 3]));
-					push (@itc_tape_used, ($info[($tape_types * $i) + 4]));
+					$itc_tape_total = ($info[(3 * $i) + 3]);
+								print "itc_tape_total = $itc_tape_total\n";
 				}
 				if ($location eq "poole") {
-					push (@poole_tape_alloc, ($info[($tape_types * $i) + 3]));
-					push (@poole_tape_used, ($info[($tape_types * $i) + 4]));
+					$poole_tape_total = ($info[(3 * $i) + 3]);
+								print "poole_tape_total = $poole_tape_total\n";
 				}
 			}
 			else {
 				if ($location eq "itc") {
-					$itc_tape_total = ($info[($tape_types * $i) + 3]);
+					push (@itc_tape_alloc, ($info[(3 * $i) + 3]));
+					push (@itc_tape_used, ($info[(3 * $i) + 4]));
 				}
 				if ($location eq "poole") {
-					$poole_tape_total = ($info[($tape_types * $i) + 3]);
+					push (@poole_tape_alloc, ($info[(3 * $i) + 3]));
+					push (@poole_tape_used, ($info[(3 * $i) + 4]));
 				}
 			}
 		}
@@ -131,31 +143,36 @@ foreach my $file (@files) {
 		# Ex.: Samqfs12, B tapes, Number Allocated = 4.3.2.0.1.12.2.1
 
 		#Break apart hostname for OID use, rebuild it for later
-		push (my @host_id, substr ($hostname, index ($hostname, '\d')));
-		push (@host_id, $hostname);
-		$hostname = $host_id[0].$host_id[1];
+		my @host_id;
+		my $id_name  = substr($hostname, 0, 6);
+		my $id_num = substr($hostname, 6, (length($hostname) - 6));
+		push (@host_id, $id_name);
+		push (@host_id, $id_num);
 		my $OID_dir;
 		#Expand this switch as necessary in the future
 		if (lc($host_id[0]) eq "samqfs") {
-			$OID_dir = $storage_dir . "/". $samqfs;
+			$OID_dir = $storage_dir . "/1";
 		}
 		elsif (lc($host_id[0]) eq "sancon") {
-			$OID_dir = $storage_dir . "/" . $sancon;
+			$OID_dir = $storage_dir . "/2";
 		}
-		$OID_dir = $OID_dir . $host_id[1];
-		for (my $i = 0; $i <= $tape_types; $i++) {
-			$OID_dir = $OID_dir . "/" . (lc(ord($tape_name[$i])) - $a_value);
-			mkdir ($OID_dir, 0755);
+		$OID_dir = $OID_dir . "/" . $host_id[1];
+		for (my $i = 0; $i < $tape_types; $i++) {
+			my $tape_val = (ord($tape_name[$i]) - $a_val_adj);
+			$OID_dir = $OID_dir . "/" . $tape_val;
+			if (!(-d $OID_dir)) {
+				system("mkdir -p $OID_dir");
+			}
 			for (my $j = 1; $j <= 2; $j++) {
 				#Allocated = 1, Used = 2
-				$OID_dir = $OID_dir . "/" . $j;
-				my $string  = "";
-				open (outfile_handle, "> $OID_dir") || die "Couldn't open $OID_dir for write: $! \n";
+				my $string;
+				my $dir_str = $OID_dir . "/" . $j;
+				open outfile_handle, "> $dir_str" or die "Couldn't open $dir_str for write: $! \n";
 				if ($j == 1) {
-					$string = join ('.', "0:", $hostname, $tape_name[$i], "Allocated:INTEGER:", $tape_alloc[$i]);
+					$string = "0:" . @host_id . "Allocated:INTEGER:" . $tape_alloc[$i];
 				}
 				else {
-					$string = join ('.', "0:", $hostname, $tape_name[$i], "Used:INTEGER:", $tape_used[$i]);
+					$string = "0:" . @host_id . "Used:INTEGER:" . $tape_used[$i];
 				}
 				print outfile_handle $string;
 				close outfile_handle;
@@ -166,28 +183,38 @@ foreach my $file (@files) {
 close infile_handle;
 
 #Compute free tape
+			print "itc_tape_total = $itc_tape_total\n";
+			print "poole_tape_total = $poole_tape_total\n";
 my $itc_free_total = $itc_tape_total;
-for (my $i = 0; $i < @itc_tape_used; $i++) {
-  $itc_free_total -= $itc_tape_used[$i];
+for (my $k = 0; $k < @itc_tape_used; $k++) {
+  $itc_free_total -= $itc_tape_used[$k];
 }
 my $poole_free_total = $poole_tape_total;
-for (my $i = 0; $i < @poole_tape_used; $i++) {
-  $poole_free_total -= $poole_tape_used[$i];
+for (my $l = 0; $l < @poole_tape_used; $l++) {
+  $poole_free_total -= $poole_tape_used[$l];
 }
 #Store free tape information
-my $string = "";
+my $o_string = "";
 #itc_tape
-my $OID_dir = "";
+my $OID_dir;
 $OID_dir = $storage_dir . $free_tape_itc;
-open (outfile_handle, "> $OID_dir") || die "Couldn't open $OID_dir for write: $! \n";
-$string = join ('.', "0:", "ITC_Free_Tape:INTEGER:", $itc_free_total);
-print outfile_handle $string;
+if (!(-d $OID_dir)) {
+	system("mkdir -p $OID_dir");
+}
+open outfile_handle, ">$OID_dir/1" or die "Couldn't open $OID_dir for write: $! \n";
+print "$itc_free_total\n";
+$o_string = join ('.', "0:", "ITC_Free_Tape:INTEGER:", $itc_free_total);
+print outfile_handle $o_string;
 close outfile_handle;
 #poole_tape
 $OID_dir = $storage_dir . $free_tape_poole;
-open (outfile_handle, "> $OID_dir") || die "Couldn't open $OID_dir for write: $! \n";
-$string = join ('.', "0:", "POOLE_Free_Tape:INTEGER:", $poole_free_total);
-print outfile_handle $string;
+if (!(-d $OID_dir)) {
+	system("mkdir -p $OID_dir");
+}
+open outfile_handle, ">$OID_dir/2" or die "Couldn't open $OID_dir for write: $! \n";
+print "$poole_free_total\n";
+$o_string = join ('.', "0:", "POOLE_Free_Tape:INTEGER:", $poole_free_total);
+print outfile_handle $o_string;
 close outfile_handle;
 
 ###########
@@ -198,26 +225,19 @@ close outfile_handle;
 # Begin Functions
 ##################
 sub error_condition {
-	switch ($_[0]) {
-		case (NAME) {
-			print "Improper file name: $_[1]\n";
-			print "Use format: <hostname>_<YYYYMMDD>\n";
-		}
-		case (DATE_FORM) {
-			print "Improper date format: $_[1]\n";
-			print "Use format: <YYYYMMDD>\n";
-		}
-		case (DATE_OUT) {
-			print "Please update file: $_[1]\n";
-			print "Today's date: $date\n";
-		}
-		case (INFO_ERR) {
-			print "Error in inputfile information: $_[1]\n";
-			print "Format is: <host>:<local>:<tapetype>:<tapes>:<usedtapes>\n";
-		}
-		case (HOSTNAME_DIF) {
-			print "Filename and hostname different: $_[1], $_[2]\n";
-			print "Should be the same\n";
-		}
+	if ($_[0] == 1) {
+			print "Improper file name: $_[1]\nUse format: <hostname>_<YYYYMMDD>\n"
+	}
+	elsif ($_[0] == 2) {
+			print "Improper date format: $_[1]\nUse format: <YYYYMMDD>\n"
+	}
+	elsif ($_[0] == 3) {
+			print "Please update file: $_[1]\nToday's date: $date\n"
+	}
+	elsif ($_[0] == 4) {
+			print "Error in inputfile information: $_[1]\nFormat is: <host>:<local>:<tapetype>:<tapes>:<usedtapes>\n"
+	}
+	elsif ($_[0] == 5) {
+			print "Filename and hostname different: $_[1], $_[2]\nShould be the same\n"
 	}
 }
